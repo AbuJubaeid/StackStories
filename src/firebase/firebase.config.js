f825fdc06236067bacd4817e25e115aaa527200e
AbuJubaeid <abujubaeidpranto2@gmail.com>
2026-06-16 18:43:51 +0600

    fixed authentication

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -9,5 +9,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export default app;
